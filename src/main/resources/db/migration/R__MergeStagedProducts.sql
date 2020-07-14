5d5cbe61d320d10d765e9933e867a12010d6cb60
Tan <jantan@38f9d3e227ba.ant.amazon.com>
2020-07-14 13:56:10 +0100

    added merge sp

--- src/main/resources/db/migration/R__MergeStagedProducts.sql
+++ src/main/resources/db/migration/R__MergeStagedProducts.sql
@@ -2,6 +2,6 @@ create or replace procedure merge_staged_products()
 as $$
 BEGIN
     update products set status='CLOSED' where product_name in (select product_name from products_staging) and status='ACTIVE';
-    insert into products select product_name, price from products_staging;
+    insert into products(product_name,price) select product_name, price from products_staging;
 END;
 $$ LANGUAGE plpgsql;
